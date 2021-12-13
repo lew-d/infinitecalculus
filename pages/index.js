78e44e8a2c07cd702229723a18be2559ff2d49ef
Lewis Carson <lcarson679@wallacehigh.net>
2021-12-13 22:56:29 +0000

    ready2

--- pages/index.js
+++ pages/index.js
@@ -160,7 +160,7 @@ export default function Home() {
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        I Ran Out Of Maths Questions 😔
+        I Ran Out Of Differentiation Questions 😔
       </footer>
     </div >
   )
