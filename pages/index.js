158a0e7d5467779215b4a6ab40b7436e8fca7905
Lewis Carson <lcarson679@wallacehigh.net>
2021-12-14 20:38:43 +0000

    ready3

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,66 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import { motion, AnimatePresence, onTap } from "framer-motion"
-import { derivative, simplify } from 'mathjs'
+import { derivative, parse, simplify } from 'mathjs'
 import { InlineMath } from 'react-katex'
 import 'katex/dist/katex.min.css';
 
-function ex(chance, base, f) {
-  // 1 in chance chance of f being called
-  if (Math.random() < chance) {
-    return f(base)
+function countInstances(string, word) {
+  return string.split(word).length - 1;
+}
+
+function replaceAllInstances(original, word, f) {
+  while (countInstances(original, word) > 0) {
+    original = original.replace(word, f())
   }
+  return original
+}
+
+function randomA() {
+  var bases = [
+    "x", "2x", "x^2"
+  ]
+
+  return `(${bases[Math.floor(Math.random() * bases.length)]})`
+}
+
+function randomB() {
+  var bases = [
+    "x-1", "x+1", "x^2", "x^2+x", "log(A)", "sin(A)", "cos(A)", "e^x", "e^2x"
+  ]
+
+  return `(${bases[Math.floor(Math.random() * bases.length)]})`
+}
+
+function generateDifferential() {
+  /*
+    where a is simple (2 terms, no e lor log)
+    b is complex (2 terms, may have e or log)
+  */
+  var bases = [
+    "A/B", "B+log(A)", "A/sin(A)", "A*cos(A)", "tan(A)/A", "B/cot(A)", "B*e^A", "e^B+B"
+  ]
+
+  // random base
+  var base = bases[Math.floor(Math.random() * bases.length)]
+
+  base = replaceAllInstances(base, "A", randomA)
+  base = replaceAllInstances(base, "B", randomB)
+
+  base = replaceAllInstances(base, "A", randomA)
+  base = replaceAllInstances(base, "B", randomB)
+
   return base
 }
 
-function GenerateEquation() {
-  var base = "x"
-
-  base = ex(0.4, base, (e) => e = e.replace("x", "log(x)"))
-
-  base = ex(0.4, base, (e) => e = e.replace("x", "(x-1)"))
-
-  //random int between 2 and 6
-  var num = Math.floor(Math.random() * 6) + 2
-  base = ex(0.8, base, (e) => e += "^" + num)
-
-  base = ex(0.7, base, (e) => e = e.replace("log(x)", "sin(x)"))
-  base = ex(0.7, base, (e) => e = e.replace("sin(x)", "cos(x)"))
-
-  base = ex(0.2, base, (e) => e = e.replace("cos(x)", "tan(x)"))
-
-  num = Math.floor(Math.random() * 6) + 2
-  base = ex(0.8, base, (e) => e += "e^(" + num + "x)")
-
-  console.log(base)
-  if (base.length < 10) {
-    var numer = "1"
-    numer = ex(0.4, numer, (e) => e = e.replace("1", "x"))
-    numer = ex(0.2, numer, (e) => e = e.replace("x", "x^2"))
-    numer = ex(0.2, numer, (e) => e = e.replace("1", "e^x"))
-    numer = ex(0.2, numer, (e) => e = e.replace("1", "cos(x)"))
-    numer = ex(0.2, numer, (e) => e = e.replace("1", "tan(x)"))
-    base = numer + "/(" + base + ")"
-  }
-  return base
+const animate = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0, y: -10 },
+  transition: { duration: 0.2 }
 }
 
-function Question({ onEnd, equation, answer }) {
+function DifferentiationQuestion({ onEnd, equation, answer }) {
   var [hideAnswer, setHideAnswer] = useState(true)
   var [answerAppeared, setAnswerAppeared] = useState(false)
 
@@ -57,39 +72,31 @@ function Question({ onEnd, equation, answer }) {
   }
 
   return (
-    <motion.div initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0, y: -10 }}
-      transition={{ duration: 0.2 }} className='text-5xl hover:cursor-pointer'>
+    <motion.div {...animate} className='text-5xl hover:cursor-pointer'>
       <AnimatePresence>
         {hideAnswer &&
-          <motion.div initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.2 }}
+          <motion.div {...animate}
             onTap={findAnswer}
             key="question"
           >
             <div className="opacity-20 pb-5 text-3xl">
               Question: (click for next)
             </div>
-            <InlineMath math={equation} />
+            <InlineMath math={"y=" + equation} />
 
           </motion.div>
         }
 
         {
           answerAppeared &&
-          <motion.div initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.2 }} onTap={() => onEnd()}
+          <motion.div  {...animate}
+            onTap={() => onEnd()}
             key="Answer"
           >
             <div className="opacity-20 pb-5 text-3xl">
               Answer: (click for next)
             </div>
-            <InlineMath math={answer} />
+            <InlineMath math={"\\frac{dy}{dx}=" + answer} />
           </motion.div>
         }
       </AnimatePresence >
@@ -97,21 +104,26 @@ function Question({ onEnd, equation, answer }) {
   )
 }
 
-function Intro({ onEnter }) {
-  //framer motion fade out
 
+function TrigonometryQuestion({ onEnd, equation }) {
   return (
-    <motion.div initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0, y: -10 }}
-      transition={{ duration: 0.2 }}
-      className='relative text-5xl hover:cursor-pointer text-blue-500' onTap={onEnter}>
-      Start Differentiating -->
-    </motion.div>
+    <motion.div {...animate} className='text-5xl hover:cursor-pointer'>
+      <AnimatePresence>
+        <motion.div {...animate}
+          onTap={() => onEnd()}
+          key="question"
+        >
+          <div className="opacity-20 pb-5 text-3xl">
+            Question: (click for next)
+          </div>
+          <InlineMath math={equation} />
+        </motion.div>
+      </AnimatePresence >
+    </motion.div >
   )
 }
 
-function eqSplit(eq) {
+function solveDiff(eq) {
   eq = simplify(eq)
   var equation = eq.toTex().replace("log", "ln")
   var answer = simplify(derivative(eq, 'x')).toTex().replace("log", "ln")
@@ -119,31 +131,102 @@ function eqSplit(eq) {
   return { equation, answer }
 }
 
+function Differentiation({ onEnd }) {
+  var { equation, answer } = solveDiff(generateDifferential())
+  return (
+    <DifferentiationQuestion
+      onEnd={onEnd}
+      equation={equation}
+      answer={answer}
+    />
+  )
+}
+
+function* trig() {
+  var initial = ["tan(x)", "cot(x)", "sin(x)", "cos(x)"]
+  //random initial
+  initial = initial[Math.floor(Math.random() * initial.length)]
+  while (true) {
+    initial = initial.replace("tan(x)", "(sin(x)/tan(x))")
+    initial = initial.replace("tan^2x", "")
+    initial = initial.replace("tan(x)", "(sin(x)/tan(x))")
+    yield initial;
+  }
+}
+
+
+function Trigonometry({ onEnd, equation, answer }) {
+  var t = trig()
+
+  // get first three values from generator
+  var [a, b, c] = [t.next().value, t.next().value, t.next().value]
+
+  return (
+    <TrigonometryQuestion
+      onEnd={onEnd}
+      equation={parse(c).toTex() + "≡" + parse(c).toTex()}
+    />
+  )
+}
+
+
+
+function Intro({ onEnter, text }) {
+  //framer motion fade out
+
+  return (
+    <motion.div {...animate}
+      className='relative text-5xl hover:cursor-pointer text-blue-500 hover:text-blue-400 transition-colors' onTap={onEnter}>
+      {text}
+    </motion.div>
+  )
+}
+
+
+
 export default function Home() {
   var [entered, setEntered] = useState(false)
-  var [questionAppeared, setQuestionAppeared] = useState(false)
+  var [diffAppeared, setDiffAppeared] = useState(false)
+  var [trigAppeared, setTrigAppeared] = useState(false)
 
-  var eq = GenerateEquation()
-  var [equation, setEquation] = useState(eqSplit(GenerateEquation()))
-
-  var enter = () => {
+  var enterDiff = () => {
     setEntered(true)
     setTimeout(() => {
-      setQuestionAppeared(true)
+      setDiffAppeared(true)
     }, 210)
   }
 
-  var onEnd = () => {
 
-    setQuestionAppeared(false)
+  var enterTrig = () => {
+    setEntered(true)
     setTimeout(() => {
-      var split = eqSplit(GenerateEquation())
-      setEquation(split)
-      setQuestionAppeared(true)
+      setTrigAppeared(true)
+    }, 210)
+  }
+
+  var onTrigEnd = () => {
+    setTrigAppeared(false)
+    setTimeout(() => {
+      setTrigAppeared(true)
+
       setEntered(true)
     }, 210)
   }
 
+  var onDiffEnd = () => {
+    setDiffAppeared(false)
+    setTimeout(() => {
+      setDiffAppeared(true)
+
+      setEntered(true)
+    }, 210)
+  }
+
+  /*
+  
+                <br />
+                <Intro key="intro" onEnter={enterTrig} text={"Trigonometric Identities -->"} />
+  */
   return (
     <div>
       <Head>
@@ -158,8 +241,19 @@ export default function Home() {
         <div className="flex h-screen">
           <div className="m-auto">
             <AnimatePresence>
-              {!entered && <Intro key="intro" onEnter={enter} />}
-              {questionAppeared && <Question key="question" onEnd={onEnd} equation={equation.equation} answer={equation.answer} />}
+              {!entered && <div>
+                <Intro key="intro" onEnter={enterDiff} text={"Differentiation -->"} />
+              </div>}
+              {diffAppeared &&
+                <Differentiation
+                  key="question"
+                  onEnd={onDiffEnd} />
+              }
+              {trigAppeared &&
+                <Trigonometry
+                  key="question"
+                  onEnd={onTrigEnd} />
+              }
             </AnimatePresence>
           </div>
         </div>
